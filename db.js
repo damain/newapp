693ecfe40e3860de3759249e33bbc644029a8137
Damain <damainjoseph@live.com>
2013-10-12 21:31:20 +0000

    added db.js

---
+++ db.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var User = new Schema({
+	email: String,
+	password: String,
+	active: Boolean,
+	fname: String,
+	lname: String
+
+});
+
+mongoose.model('User', User);
+Mongoose.connect("mongodb://xalon:proto1@ds049888.mongolab.com:49888/xalon");
